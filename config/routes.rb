14714c424275e20b1b7702055694f42144033555
faithpst <faithpst@163.com>
2013-09-05 00:16:12 +0800

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  # The priority is based upon order of creation: first created -> highest priority.
+  get "users/new"
+  root 'static_pages#home' 
+  match '/help',    to: 'static_pages#help',       via: 'get'
+  match '/about',   to: 'static_pages#about',      via: 'get'
+  match '/contact', to: 'static_pages#contact',    via: 'get'
+  match '/signup',  to: 'users#new',               via: 'get'
+
+  # root_path -> '/'
+  # root_url  -> 'http://localhost:3000/'
+   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
